f45f96c1e88be19f7349956d89c298b5d6bcfd8d
zhaojinzhong923 <1046576137@qq.com>
2022-03-24 22:27:20 +0800

    2-6 开发hello world接口

--- src/main/java/com/zhong/wiki/WikiApplication.java
+++ src/main/java/com/zhong/wiki/WikiApplication.java
@@ -1,13 +1,27 @@
 package com.zhong.wiki;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
+import java.util.logging.Logger;
+
+@ComponentScan({"com.zhong"})
 @SpringBootApplication
 public class WikiApplication {
 
+//    private static final Logger LOG=  LoggerFactory.getLogger(WikiApplication.class);
+
     public static void main(String[] args) {
+
         SpringApplication.run(WikiApplication.class, args);
+
+//        SpringApplication app=new SpringApplication(WikiApplication.class);
+//        Environment env=app.run(args).getEnvironment();
+//        LOG.info("启动成功");
+//        LOG.info("地址： \thttp://127.0.0.1:{}");
     }
 
 }
